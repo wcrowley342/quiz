45f2159d65e788ff71f3b343cfbe5c66b965bed2
Will Crowley <71472020+wcrowley342@users.noreply.github.com>
2025-06-08 11:19:47 -0600

    Create tailwind.config.js

---
+++ app/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0A2540',
+        aqua: '#02D2FF',
+        light: '#F5F7FA',
+      },
+    },
+  },
+  plugins: [],
+};
